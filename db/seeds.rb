422cc062d9e2b62b1c8d8f467f3e69f79347cf57
Gregoire Courtois <courtoisgregoire@gmail.com>
2019-02-13 16:37:53 +0100

    reviews and restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+    {
+      name: 'Bamboleio',
+      address: 'La casa',
+      category: 'italian',
+      phone_number: '0102030405'
+    },
+    {
+      name: 'Yokoso',
+      address: 'Paris 7eme',
+      category: 'japanese',
+      phone_number: '9874561231'
+    },
+    {
+      name: 'Le vin rouge',
+      address: 'Central Park, NY',
+      category: 'french',
+      phone_number: '3318211068'
+    },
+    {
+      name: 'Casa de Papa',
+      address: "Place d'italie, Paris 13",
+      category: 'italian',
+      phone_number: '0123456789'
+    },
+    {
+      name: 'The yummy Mao',
+      address: 'Rue Oberkampf, Paris',
+      category: 'chinese',
+      phone_number: '0344553355'
+    }
+  ]
+)
